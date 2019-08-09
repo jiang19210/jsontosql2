ae4dc2976db5dab1685570c1d26269c0652231ac
姜光明 <jiangguangming@u51.com>
2019-08-09 10:41:47 +0800

    update readme

--- README.md
+++ README.md
@@ -7,13 +7,17 @@ var index = require('jsontosql2');
 var collection = {
     'name': 'OrderInfo',
     'data': [{
-        'Id': 1,
-        'Pirce': 1
-    }, {
-        'Id': 2,
-        'Pirce': 2
-    }],
-    'duplicate': ['Pirce']
+        'Id': 1
+    },
+        {
+            'Id': 1,
+            'Pirce': 1
+        }, {
+            'Id': 2,
+            'Pirce': 2
+        }],
+    'duplicate': ['Pirce'],
+    'columns': ['Id', 'Pirce']
 };
 var sql = index.toInsertSql(collection);
 console.log('JsonToInsertSql，使用mysql duplicate关键字：' + sql);
--- test/test.js
+++ test/test.js
@@ -1,3 +1,18 @@
-var test = require('../index');
-var d = test.toInsertSql({'name':'a','data':[{'a':'a','b':null},{'a':'a','b':'b','c':'c'}],'duplicate':['a','c'],'columns':['a','b','c','d']});
-console.log(d)
+var index = require('../index');
+var collection = {
+    'name': 'OrderInfo',
+    'data': [{
+        'Id': 1
+    },
+        {
+            'Id': 1,
+            'Pirce': 1
+        }, {
+            'Id': 2,
+            'Pirce': 2
+        }],
+    'duplicate': ['Pirce'],
+    'columns': ['Id', 'Pirce']
+};
+var sql = index.toInsertSql(collection);
+console.log('JsonToInsertSql，使用mysql duplicate关键字：' + sql);
